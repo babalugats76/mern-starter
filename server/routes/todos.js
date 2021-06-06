3e78dd2f4ece205ddd1a85bbd104418cd15a9b42
James Colestock <james@colestock.com>
2021-06-06 14:16:36 -0600

    added post route handler

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -11,4 +11,22 @@ module.exports = (app) => {
       next(e);
     }
   });
+
+  app.post("/api/todos", async (req, res, next) => {
+    try {
+      const { id, userId, title, completed } = req.body;
+
+      // Create todo record
+      const todo = await new Todo({
+        id,
+        userId,
+        title,
+        completed,
+      }).save();
+
+      res.send({ todo });
+    } catch (e) {
+      next(e);
+    }
+  });
 };
